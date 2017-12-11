e1ddc7b85ebaeb4f33107103e1128ee9f3180bbf
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-11 16:00:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,9 @@ def fizzbuzz(x)
     "Fizz"
   elsif x % 5 == 0
     "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
-    "FizzBuzz"
+  elsif ( x % 3 == 0 ) && ( x % 5 == 0)
+    
+      "FizzBuzz"
+    
   end
 end
